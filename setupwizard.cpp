3ea1d0cbdfc9800bcef24cf634aadb083adf0ba3
Sclafus <alessandro.sclafani00@gmail.com>
2022-06-15 14:06:19 +0200

    SetupWizard: gathering data

--- setupwizard.cpp
+++ setupwizard.cpp
@@ -2,19 +2,34 @@
 #include "ui_setupwizard.h"
 
 SetupWizard::SetupWizard(QWidget *parent) :
-  QWizard(parent),
-  ui(new Ui::SetupWizard)
+	QWizard(parent),
+	ui(new Ui::SetupWizard)
 {
-  ui->setupUi(this);
+	ui->setupUi(this);
 }
 
 SetupWizard::~SetupWizard()
 {
-  delete ui;
+	delete ui;
 }
 
-void SetupWizard::on_SetupWizard_finished(int result)
-{
-    qDebug() << "done" << result;
-}
+void SetupWizard::on_SetupWizard_finished(int result) {
+	if (result == 1) {
+		QString gosuIp = ui->gosumemoryIpLineEdit->text();
+		int gosuPort = ui->gosumemoryPortLineEdit->text().toInt();
 
+		QString twitchChannel = ui->twitchChannelLineEdit->text();
+		QString twitchBotNick = ui->twitchBotNickLineEdit->text();
+		QString twitchOauth = ui->twitchOauthLineEdit->text();
+
+		QString osuircServer = ui->osuircServerLineEdit->text();
+		int osuircPort = ui->osuircPortLineEdit->text().toInt();
+		QString osuircNick = ui->osuircNickLineEdit->text();
+		QString osuircPassword = ui->osuircPasswordLineEdit->text();
+
+		qDebug() << "gosu" << gosuIp << gosuPort;
+		qDebug() << "twitch" << twitchBotNick << twitchChannel << twitchOauth;
+		qDebug() << "osuirc" << osuircNick << osuircServer << osuircPort << osuircPassword;
+	}
+	qDebug() << "done" << result;
+}
