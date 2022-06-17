226adcd4a99023bdd825c2a9e8fe1e4cb8978f16
Sclafus <alessandro.sclafani00@gmail.com>
2022-06-17 20:52:00 +0200

    Main: added org and app name to use qsettings consistently

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@
 
 int main(int argc, char **argv) {
 	QApplication app(argc, argv);
+	app.setOrganizationName("miraya");
+	app.setOrganizationDomain("github.com/MirayaProject");
+	app.setApplicationName("bot");
 	MainWindow mw;
-	QSettings settings("miraya", "bot");
 	mw.show();
 	return app.exec();
 }
