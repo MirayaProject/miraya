d94a334ddf7f780ecf51afe0b5c0f9b7729e351a
Sclafus <alessandro.sclafani00@gmail.com>
2022-06-17 20:44:52 +0200

    🟣 Twitch: added websocket based client for twitch chat

---
+++ twitchClient.cpp
@@ -0,0 +1,69 @@
+#include "twitchClient.h"
+
+TwitchClient::TwitchClient(
+	const QUrl &url,
+	const QString &botNick,
+	const QString &oauth,
+	const QString &channel,
+	QObject *parent
+) : QObject(parent), url(url), oauth(oauth), botNick(botNick), channel(channel) {
+	connect(&socket, &QWebSocket::connected, this, &TwitchClient::onConnected);
+	connect(&socket, &QWebSocket::textMessageReceived, this, &TwitchClient::onTextMessageReceived);
+	connect(&socket, &QWebSocket::disconnected, this, &TwitchClient::onDisconnected);
+}
+
+
+void TwitchClient::init(){
+	qDebug() << "Connecting to: " << url.toString();
+	socket.open(QUrl(url));
+}
+
+
+void TwitchClient::sendChatMessage(QString message)
+{
+	qDebug() << "Sending channel message: " << message;
+	socket.sendTextMessage("PRIVMSG #" + channel + " :" + message);
+}
+
+
+void TwitchClient::sendMessage(QString message)
+{
+	qDebug() << "Sending message: " << message;
+	socket.sendTextMessage(message);
+}
+
+
+void TwitchClient::onConnected() {
+	qDebug() << "Connected to: " << url.toString();
+	emit connected();
+	sendMessage("PASS " + oauth);
+	sendMessage("NICK " + botNick);
+	sendMessage("JOIN #" + channel);
+}
+
+
+void TwitchClient::onTextMessageReceived(QString message){
+	qDebug() << "Message received from: " << url.toString() << message;
+	emit textMessageReceived(message);
+}
+
+
+void TwitchClient::onDisconnected(){
+	qDebug() << "Disconnected from:" << url.toString();
+	emit disconnected();
+}
+
+
+void TwitchClient::setChannel(QString channel) {
+	this->channel = channel;
+}
+
+
+void TwitchClient::setBotNick(QString botNick) {
+	this->botNick = botNick;
+}
+
+
+void TwitchClient::setOauth(QString oauth) {
+	this->oauth = oauth;
+}
---
+++ twitchClient.h
@@ -0,0 +1,47 @@
+#ifndef TWITCHCLIENT_H
+#define TWITCHCLIENT_H
+
+#include <QObject>
+#include <QString>
+#include <QUrl>
+#include <QDebug>
+
+#include <QWebSocket>
+
+class TwitchClient : public QObject {
+	Q_OBJECT
+
+	public:
+		explicit TwitchClient(
+			const QUrl &url,
+			const QString &botNick,
+			const QString &oauth,
+			const QString &channel,
+			QObject *parent = nullptr
+		);
+		void init();
+		void setChannel(QString channel);
+		void setBotNick(QString botNick);
+		void setOauth(QString oauth);
+		void sendChatMessage(QString message);
+
+	private:
+		void sendMessage(QString message);
+		QWebSocket socket;
+		QUrl url;
+		QString oauth;
+		QString botNick;
+		QString channel;
+
+	signals:
+		void connected();
+		void textMessageReceived(QString message);
+		void disconnected();
+
+	private slots:
+		void onConnected();
+		void onTextMessageReceived(QString message);
+		void onDisconnected();
+};
+
+#endif
