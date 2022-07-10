8df84195a42128b3bcc680bd872f1d50070a0e10
Sclafus <alessandro.sclafani00@gmail.com>
2022-07-10 23:00:01 +0200

    🟢 irc: added method for map requests

--- osuircclient.cpp
+++ osuircclient.cpp
@@ -12,13 +12,15 @@ OsuIrcClient::OsuIrcClient(
 	connect(&socket, &QTcpSocket::disconnected, this, &OsuIrcClient::onDisconnected);
 }
 
+
 void OsuIrcClient::init(){
 	socket.connectToHost(server, port);
 	socket.write("PASS " + password.toLocal8Bit() + " \r\n");
 	socket.write("USER " + nickname.toLocal8Bit() + " " + nickname.toLocal8Bit() + " " + nickname.toLocal8Bit() + " :" + nickname.toLocal8Bit() + " \r\n");
 	socket.write("NICK " + nickname.toLocal8Bit() + " \r\n");
-    sendMsg("Ready to process requests");
+	sendMsg("Ready to process requests");
 }
+
 
 void OsuIrcClient::onConnected()
 {
@@ -26,13 +28,32 @@ void OsuIrcClient::onConnected()
 	emit connected();
 }
 
+
 void OsuIrcClient::onDisconnected()
 {
 	qDebug() << "Disconnected from: " << socket.peerAddress().toString();
 	emit disconnected();
 }
 
+
 void OsuIrcClient::sendMsg(QString message)
 {
 	socket.write("PRIVMSG " + nickname.toLocal8Bit() + " " + message.toLocal8Bit() + " \r\n");
+}
+
+
+void OsuIrcClient::sendMap(QUrl url, TwitchDataWrapper message)
+{
+	// TODO: the map embed should have the map name.
+	// Scraping is needed.
+
+	qDebug() << "Sending map: " << url.toString() << " by " << message.getUsername();
+
+	socket.write(
+		"PRIVMSG "
+		+ nickname.toLocal8Bit()
+		+ " [" + url.toString().toLocal8Bit() + " Map request] "
+		+ "sent by " + message.getUsername().toLocal8Bit()
+		+ " \r\n"
+	);
 }
