cd4a05e99d3f249fc68ebbbc5aca4cff03cd0314
Sclafus <alessandro.sclafani00@gmail.com>
2023-05-28 20:33:43 +0200

    🔵 commands: added custom commands widget

--- src/commands/commandswindow.h
+++ src/commands/commandswindow.h
@@ -2,21 +2,30 @@
 #define COMMANDSWINDOW_H
 
 #include <QDialog>
+#include <QMessageBox>
+#include <QSettings>
 
 namespace Ui {
-class CommandsWindow;
+	class CommandsWindow;
 }
 
 class CommandsWindow : public QDialog
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit CommandsWindow(QWidget *parent = nullptr);
-    ~CommandsWindow();
+	explicit CommandsWindow(QWidget *parent = nullptr);
+	~CommandsWindow();
 
 private:
-    Ui::CommandsWindow *ui;
+  void on_addCommandBtn_clicked();
+  void on_removeCommandBtn_clicked();
+	void on_saveBtn_clicked();
+
+	void loadData();
+	void addRow(QString command, QString response);
+
+	Ui::CommandsWindow *ui;
 };
 
 #endif // COMMANDSWINDOW_H
