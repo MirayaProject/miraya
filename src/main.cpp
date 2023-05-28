256ecf7cdeb58d8fe1072affe9febe47be5bceca
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2023-05-28 20:28:21 +0200

    🪩 main: v1.1.1

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
 	app.setOrganizationName("miraya");
 	app.setOrganizationDomain("github.com/MirayaProject");
 	app.setApplicationName("bot");
-	app.setApplicationVersion("1.1.0");
+	app.setApplicationVersion("1.1.1");
 	MainWindow mw;
 	mw.show();
 	return app.exec();
