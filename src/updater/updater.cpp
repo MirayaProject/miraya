1a3b7ee15d511b567e98e8b9cedc62916965a7e7
Sclafus <alessandro.sclafani00@gmail.com>
2023-05-29 17:10:52 +0200

    🤓 updater: added implementation

---
+++ src/updater/updater.cpp
@@ -0,0 +1,42 @@
+#include "updater.h"
+
+Updater::Updater(QObject *parent)
+	: QObject{parent}
+{
+
+}
+
+void Updater::checkVersion()
+{
+	qDebug() << "[Updater] starting version check";
+	QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+	connect(manager, &QNetworkAccessManager::finished, this, &Updater::onReplyFinished);
+
+	QUrl url("https://api.github.com/repos/MirayaProject/miraya/releases/latest");
+	QNetworkRequest request(url);
+	request.setHeader(QNetworkRequest::UserAgentHeader, "Miraya");
+	manager->get(request);
+	qDebug() << "[Updater] version check request sent";
+}
+
+void Updater::onReplyFinished(QNetworkReply *reply)
+{
+	qDebug() << "[Updater] version reply received";
+	if (reply->error() != QNetworkReply::NoError){
+		return;
+	}
+
+	QByteArray responseData = reply->readAll();
+	QJsonDocument jsonDoc = QJsonDocument::fromJson(responseData);
+	QJsonObject jsonObj = jsonDoc.object();
+
+	QString latestVersionStr = jsonObj.value("tag_name").toString();
+	QVersionNumber latestVersion = QVersionNumber::fromString(latestVersionStr);
+	QVersionNumber currentVersion = QVersionNumber::fromString(QCoreApplication::applicationVersion());
+
+	if(currentVersion >= latestVersion){
+		qDebug() << "[Updater] already running on latest or pre-release version";
+		return;
+	}
+	emit newVersionAvailable();
+}
---
+++ src/updater/updater.h
@@ -0,0 +1,29 @@
+#ifndef UPDATER_H
+#define UPDATER_H
+
+#include <QCoreApplication>
+#include <QDebug>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QObject>
+#include <QVersionNumber>
+
+class Updater : public QObject
+{
+	Q_OBJECT
+public:
+	explicit Updater(QObject *parent = nullptr);
+	void checkVersion();
+
+signals:
+	void newVersionAvailable();
+
+private slots:
+	void onReplyFinished(QNetworkReply *reply);
+
+};
+
+#endif // UPDATER_H
