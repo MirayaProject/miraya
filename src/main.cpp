21ddc01d625f5d373f9faf093ad88f5a6d5447da
Sclafus <alessandro.sclafani00@gmail.com>
2023-06-12 20:43:38 +0200

    🪩 main: v1.2.0-alpha.4

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
 	app.setOrganizationName("miraya");
 	app.setOrganizationDomain("github.com/MirayaProject");
 	app.setApplicationName("bot");
-	app.setApplicationVersion("1.2.0-alpha.3");
+	app.setApplicationVersion("1.2.0-alpha.4");
 	app.setWindowIcon(QIcon(":/resources/logo/logo.png"));
 	MainWindow mw;
 	mw.show();
