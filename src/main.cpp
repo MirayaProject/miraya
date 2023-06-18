158050cb33593efd123035462950291c8b07ac0a
Sclafus <alessandro.sclafani00@gmail.com>
2023-06-18 11:57:36 +0200

    🪩 main: added check for linux icon

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@ int main(int argc, char **argv) {
 	app.setOrganizationName("miraya");
 	app.setOrganizationDomain("github.com/MirayaProject");
 	app.setApplicationName("bot");
-	app.setApplicationVersion("1.2.0-alpha.4");
-	app.setWindowIcon(QIcon(":/resources/logo/logo.png"));
+	#ifdef Q_OS_LINUX
+		app.setWindowIcon(QIcon(":/resources/logo/logo.png"));
+	#endif
 	MainWindow mw;
 	mw.show();
 	return app.exec();
